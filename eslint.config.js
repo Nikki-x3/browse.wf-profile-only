126860fb6138d80fae2c2566d3af768f0204996d
Nikki-x3 <xo-nikki-xo@pm.me>
2026-08-27 20:47:37 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ const tsPlugin = require("@typescript-eslint/eslint-plugin");
 
 module.exports = [
   {
-    files: ["./public/*.ts"],
+    files: ["public/*.ts"],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
