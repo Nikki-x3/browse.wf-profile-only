1345cc1c774ef0aff21f527c0368c150025d1ab1
Nikki-x3 <xo-nikki-xo@pm.me>
2026-08-27 23:35:41 +0200

    Update profile.js

--- typestripped/profile.js
+++ typestripped/profile.js
@@ -183,10 +183,11 @@ function loadProfile(file) {
 
 
 function fetchProfileFromAPI() {
-    const apiURL = 'https://cors.io/?url=https://api.warframe.com/cdn/getProfileViewingData.php?playerId=51fbaece1a4d80694900000c'; 
+    const apiURL = 'https://proxy.cors.sh/https://api.warframe.com/cdn/getProfileViewingData.php?playerId=51fbaece1a4d80694900000c'; 
 
-    fetch(apiURL)
-        .then(response => {
+    fetch(apiURL, {
+          headers: { "x-cors-api-key": "live_d2b733c6adbcffcd68c836b89ccf5ef6e9e3db4244bbdeea" },
+    }).then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
