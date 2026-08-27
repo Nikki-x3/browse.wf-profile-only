be108998c3ecaa9f9018e89e2a838eaea674b451
Nikki-x3 <xo-nikki-xo@pm.me>
2026-08-27 20:17:19 +0200

    fixed verceljson

--- components/profile.php
+++ components/profile.php
@@ -4,7 +4,7 @@
 	<title>Profile Viewer | browse.wf</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-	<link rel="icon" href="wf.ico">
+	<link rel="icon" href="../wf.ico">
 	<style>
 		.form-select
 		{
